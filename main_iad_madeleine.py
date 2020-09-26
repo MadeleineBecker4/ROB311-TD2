2995561ecb86d73a82c7d342c73785565d73ae2a
iad-ABDUL-RAOUF <iad_abdulraouf@yahoo.fr>
2020-09-26 19:30:24 +0200

    add comment to main

--- main_iad_madeleine.py
+++ main_iad_madeleine.py
@@ -3,11 +3,11 @@ import numpy as np
 
 # MAIN
 
-# entree
+# environnement parameters
 R = [0, 0, 1, 10]
 x = 0.25
 y = 0.25
-# T = [[[]]*3]*4
+
 T = np.zeros((4, 3, 4))
 T[0][0] = [0, 0, 0, 0]
 T[0][1] = [0, 1, 0, 0]
@@ -24,23 +24,27 @@ T[2][2] = [0, 0, 0, 0]
 T[3][0] = [1, 0, 0, 0]
 T[3][1] = [0, 0, 0, 0]
 T[3][2] = [0, 0, 0, 0]
+
+# iteration value algorithm parameters
 gamma = 0.9
 eps = 0.00001
 
 
-# var intermediaire
+# compute the possible action for each state
 possible_Pi = af.find_possible_Pi(T)
-# var init
+
+# iteration value algorithm to find the optimal policy
+# init
 n_state = len(T)
 curr_V = [0]*n_state
 next_V = [0]*n_state
 i = 0
 diff = eps + 1
-
+# compute V*
 while diff >= eps:
     next_V = af.update_V(curr_V, gamma, T, R, possible_Pi)
     diff = af.RMS_error(curr_V, next_V)
     curr_V = next_V.copy()
     i = i+1
-
+# find the optimal policy
 optimal_Pi = af.optimal_policy(curr_V, T, possible_Pi)
